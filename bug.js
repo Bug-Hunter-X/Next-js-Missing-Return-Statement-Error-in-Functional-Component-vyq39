ce23b6757eda49099f9f064bcb665693fac978cc
BugHunter-X <askzenai@gmail.com>
2025-02-14 15:40:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,22 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href='/about'>
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause an error because of the missing return statement
+}
+```
